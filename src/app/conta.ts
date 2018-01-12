66f58a292dd47aaea34843ac7b7474b7ced2aab6
Maisa Souza <maisa.horta@gmail.com>
2018-01-12 16:30:55 -0200

    inclusao da leitura bradesco + calculo dos totais com rateio + funcionalidade local de exclusao

---
+++ src/app/conta.ts
@@ -0,0 +1,19 @@
+export class Conta {
+    referencia: Date; //Referencia da fatura de pagamento -- sempre sera considerado dia 1
+    data: Date; //Data da compra
+    valor: number;
+    descricao: string;
+    responsavel?: string;
+    valorEmDolar?: number;
+    categorias?: string[];
+    numeroParcela?: number;
+    totalParcels?: number;
+
+    printConta() {
+        console.log('Referencia: ' + this.referencia);
+        console.log('Data: ' + this.data);
+        console.log('Descricao: ' + this.descricao);
+        console.log('Valor: ' + this.valor);
+        console.log('Responsavel: ' + this.responsavel);
+    }
+}
--- src/app/nova-fatura/nova-fatura.component.ts
+++ src/app/nova-fatura/nova-fatura.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { forEach } from '@angular/router/src/utils/collection';
+import { Conta } from '../conta';
 
 @Component({
   selector: 'app-nova-fatura',
@@ -7,18 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovaFaturaComponent implements OnInit {
 
-  bancos = ['BB', 'Bradesco', 'Itau', 'Avulsos'];
+  bancos = ['Bradesco', 'BB', 'Itau', 'Avulsos'];
+  rateio = ['Maisa', 'Elias', 'Rateio'];
   referencia: string;
   bancoSelecionado: string;
   faturaCarregada = false;
   itensFatura = [];
   self = this;
+  totalConta = 0;
+
+  resumoConta = {
+    totalConta: 0,
+    restanteNaoAtribuido: 0,
+    totalMaisa: 0,
+    totalElias: 0
+  };
 
 
   constructor() { }
 
   ngOnInit() {
     this.bancoSelecionado = this.bancos[0];
+  }
+
+  reiniciaVariaveis() {
+    this.resumoConta = {
+      totalConta: 0,
+      restanteNaoAtribuido: 0,
+      totalMaisa: 0,
+      totalElias: 0
+    };
   }
 
   carregarFatura() {
@@ -31,6 +51,10 @@ export class NovaFaturaComponent implements OnInit {
     const fileReader = new FileReader();
     fileReader.onload = function () {
       const conteudo = fileReader.result;
+      console.log(self.bancoSelecionado)
+      if (self.bancoSelecionado === 'Bradesco') {
+        self.preencherFaturaBradesco(conteudo, self);
+      }
       self.preencherItensFatura(conteudo);
     };
 
@@ -38,9 +62,77 @@ export class NovaFaturaComponent implements OnInit {
   }
 
 
+  preencherFaturaBradesco(conteudoArquivo, self) {
+    const listaValores = conteudoArquivo.split('\n');
+
+    listaValores.forEach(function(linha, ind, arr){
+      if (self.isLinhaValida(linha)) {
+        const camposDaLinha = linha.split(/\s{10,50}/);
+
+        const novaConta = new Conta();
+        const mesReferencia = self.referencia.substring(0, 2) - 1;
+        const anoReferencia = self.referencia.substring(3, 7);
+        const mesCompra = linha.substring(3, 5) - 1;
+        const anoCompra = (mesCompra <= mesReferencia) ? anoReferencia : anoReferencia - 1;
+
+        novaConta.referencia = new Date(anoReferencia, mesReferencia, 1);
+        novaConta.data = new Date(anoCompra, mesCompra, linha.substring(0, 2));
+        novaConta.descricao = camposDaLinha[1];
+        novaConta.valorEmDolar = Number(camposDaLinha[2].replace('.', '').replace(',', '.'));
+        novaConta.valor = Number(camposDaLinha[3].replace('.', '').replace(',', '.'));
+
+        self.itensFatura.push(novaConta);
+        self.totalConta += novaConta.valor;
+      }
+    });
+
+    self.calcularTotais();
+  }
+
+  isLinhaValida (linha) {
+    const data = linha.substring(0, 5);
+    return (data.match('[0-9][0-9]/[0-1][0-9]'))
+  }
+
+
   preencherItensFatura = (conteudoArquivo) => {
-      const listaValores = conteudoArquivo.split('/n');
-      console.log('Length: ' + listaValores.length );
-      console.log(listaValores);
+      // const listaValores = conteudoArquivo.split('/n');
+      // console.log('Length: ' + listaValores.length );
+  }
+
+  excluirItemFatura(itemCompra) {
+
+    const index = this.itensFatura.indexOf(itemCompra);
+
+    if (index === -1) {
+      console.log('ERRO - Não deveria excluir uma compra que nao está na lista');
+    } else {
+      this.itensFatura.splice(index, 1);
+      this.calcularTotais();
+    }
+  }
+
+  calcularTotais() {
+    console.log('Entrei no calcular totais');
+    this.reiniciaVariaveis();
+    const self = this;
+    this.itensFatura.forEach(function(element, ind, arr){
+      self.resumoConta.totalConta += element.valor;
+
+      switch (element.responsavel) {
+        case 'Elias':
+          self.resumoConta.totalElias += element.valor;
+          break;
+        case 'Maisa':
+          self.resumoConta.totalMaisa += element.valor;
+          break;
+        case 'Rateio':
+          self.resumoConta.totalElias += element.valor / 2;
+          self.resumoConta.totalMaisa += element.valor / 2;
+          break;
+      }
+
+      self.resumoConta.restanteNaoAtribuido = self.resumoConta.totalConta - self.resumoConta.totalElias - self.resumoConta.totalMaisa;
+    });
   }
 }
